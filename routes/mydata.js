ae2e68e6038e49946551a106db736684e1ed50c2
SidharthaGundarapu <98143998+SidharthaGundarapu@users.noreply.github.com>
2022-03-30 23:04:33 -0500

    added new endpoint

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Sidhartha Gundarapu' });
+  res.render('mydata', { title: 'Sidhartha Gundarapu' });
 });
 
 module.exports = router;
