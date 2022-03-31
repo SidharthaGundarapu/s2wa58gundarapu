cfa5c45e8836f91d313c51d097c1ee47741038c7
SidharthaGundarapu <98143998+SidharthaGundarapu@users.noreply.github.com>
2022-03-31 00:00:23 -0500

    Added Bonus

--- routes/computation.js
+++ routes/computation.js
@@ -7,10 +7,11 @@ router.get('/', function(req, res, next) {
   var y1 = Math.abs(x);
   var y2 = Math.acos(x);
   var y3 = Math.sin(x);
+  var y4 = Math.sinh(x);
   function getRandomArbitrary(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
   }
-  res.send(`Math.abs(${x}) is ${y1} ; Math.acos(${x}) is ${y2} ; Math.sin(${x}) is ${y3}\n`);
+  res.send(`Math.abs(${x}) is ${y1} ; Math.acos(${x}) is ${y2} ; Math.sin(${x}) is ${y3}; Math.sinh(${x}) is ${y4}\n`);
 });
 
 module.exports = router;
