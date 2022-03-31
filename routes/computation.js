316858d2ecf6af6da68530c4c3d0dc7b7d3617aa
SidharthaGundarapu <98143998+SidharthaGundarapu@users.noreply.github.com>
2022-03-30 23:46:25 -0500

    Added Bonus

---
+++ routes/computation.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  const x= req.query.x || getRandomArbitrary(0, 60);
+  var y1 = Math.abs(x);
+  var y2 = Math.acos(x);
+  var y3 = Math.sin(x);
+  function getRandomArbitrary(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+  res.send(`Math.abs(${x}) is ${y1} ; Math.acos(${x}) is ${y2} ; Math.sin(${x}) is ${y3}\n`);
+});
+
+module.exports = router;
